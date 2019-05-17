4848b8f2b03c6a0b0704a8961cb57a1ab6d54a13
smzm <smzmrad@gmail.com>
2019-05-17 10:38:29 +0430

    Update

--- install.sh
+++ install.sh
@@ -865,33 +865,15 @@ done
 
         echo " ===================== i3 installation ====================="
         echo -e " i3ipc \n Feh \n arandr \n thunar \n rofi \n compton \n lxappearance \n pavuControl \n Font Awesome \n polybar \n i3lock-fancy \n redshift \n udevil \n scrot \n xclip \n Background Picture \n Gnome Calculator \n xfce4 Manager \n xautolock \n hibernator \n clipit \n ntp \n mediainfo \n unclutter \n ntfs-3g \n exfat-utils \n sxiv \n numlockx"  
-        read -p " zathura (PDF)    :::::    [r]un : " ansZathura
-        if [[ $ansZathura == "r" ]] || [[ $ansZathura == "R" ]]; then
-            echo -e "\n +++++ sudo pacman -S zathura ... \n"
-            sudo pacman -S zathura
-            sudo pacman -S zathura-pdf-mupdf
-            sudo pacman -S zathura-djvu
-            sudo pacman -S zathura-cb
-            sudo pacman -S zathura-pdf-poppler
-            sudo pacman -S zathura-ps 
+        read -p " okular (PDF)    :::::    [r]un : " ansOkular
+        if [[ $ansOkular == "r" ]] || [[ $ansOkular == "R" ]]; then
+            echo -e "\n +++++ sudo pacman -S okular ... \n"
+            sudo pacman -S okular
         fi
     clear
 
         echo " ===================== i3 installation ====================="
-        echo -e " i3ipc \n Feh \n arandr \n thunar \n rofi \n compton \n lxappearance \n pavuControl \n Font Awesome \n polybar \n i3lock-fancy \n redshift \n udevil \n scrot \n xclip \n Background Picture \n Gnome Calculator \n xfce4 Manager \n xautolock \n hibernator \n clipit \n ntp \n mediainfo \n unclutter \n ntfs-3g \n exfat-utils \n sxiv \n numlockx \n zathura"   
-        read -p " i3wm config file copy    :::::    [r]un : " ansI3Config
-        if [[ $ansI3Config == "r" ]] || [[ $ansI3Config == "R" ]]; then
-            echo -e "\n +++++ copy i3wm configuration file ... \n"
-            cp -r $dotfiles/.config/i3  $HOME/.config/
-        fi
-    clear
-
-
-
-fi
-
-        echo " ===================== i3 installation ====================="
-        echo -e " i3ipc \n Feh \n arandr \n thunar \n rofi \n compton \n lxappearance \n pavuControl \n Font Awesome \n polybar \n i3lock-fancy \n redshift \n udevil \n scrot \n xclip \n Background Picture \n Gnome Calculator \n xfce4 Manager \n xautolock \n hibernator \n clipit \n ntp \n mediainfo \n unclutter \n ntfs-3g \n exfat-utils \n sxiv \n numlockx"  
+        echo -e " i3ipc \n Feh \n arandr \n thunar \n rofi \n compton \n lxappearance \n pavuControl \n Font Awesome \n polybar \n i3lock-fancy \n redshift \n udevil \n scrot \n xclip \n Background Picture \n Gnome Calculator \n xfce4 Manager \n xautolock \n hibernator \n clipit \n ntp \n mediainfo \n unclutter \n ntfs-3g \n exfat-utils \n sxiv \n numlockx \n okular"   
         read -p " i3wm config file copy    :::::    [r]un : " ansI3Config
         if [[ $ansI3Config == "r" ]] || [[ $ansI3Config == "R" ]]; then
             echo -e "\n +++++ copy i3wm configuration file ... \n"
