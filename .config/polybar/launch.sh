ad6bd339886e75522ad61e561666bc01951ae4a2
Saeed <39596095+smzm@users.noreply.github.com>
2022-08-30 15:14:42 +0430

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,24 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload -l example &
-fi
-
-
-# Launch bar1 and bar2
-#polybar bar1 &
-#polybar bar2 &
-#polybar example -r & 
-
-echo "Bars launched..."
+polybar main --config=~/.config/polybar/config.ini &
+polybar bottom  --config=~/.config/polybar/config.ini &
