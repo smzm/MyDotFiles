8a01870f6e7b3cbe808a2b31ff4ae279dcda3436
Saeed <39596095+smzm@users.noreply.github.com>
2022-09-12 19:57:41 +0430

    Add files via upload

---
+++ .config/polybar/scripts/ipGeoLocation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+IP=`curl -w "\n" -s https://api.ipify.org`
+country=`geoiplookup "$IP" | awk 'BEGIN{FS=":"}; {print $2}' | awk 'BEGIN{FS=","}; {gsub(/[[:blank:]]/,""); print $1}'`
+#flag=`jq '.[] | select(.iso=='"$country"') .flag' ~/country.json 2> /dev/null` 
+#clean_flag=`echo "$flag" | awk '{gsub(/"/, "", $0) ; print $0}'`
+echo "$country" || echo "offline"
