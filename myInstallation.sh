fd67067a2b0bf1f92a4217ff71da4a3aeb15d830
Saeed <39596095+smzm@users.noreply.github.com>
2022-09-19 15:20:16 +0000

    Update myInstallation.sh

--- myInstallation.sh
+++ myInstallation.sh
@@ -195,16 +195,22 @@ tar xvf ~/.icons/macOSMonterey.tar.gz
 rm ~/.icons/macOSMonterey.tar.gz
 
 
-# openvpn3
+read -p "If you have any partion swap for enabling hibernate type [y]  : " anshibernate
+if [[ $anshibernate == "y" ]] || [[ $anshibernate == "Y" ]]; then
+ sudo blkid | grep swap | awk 'BEGIN{FS="UUID="};{print $2}' | awk 'BEGIN{FS="\""}; {print $2}' | xargs echo |xargs -I {} sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*/,/"$/s/"$/ resume=UUID={}"/' /etc/default/grub
+ grub-mkconfig -o /boot/grub/grub.cfg
+ sudo sed -i '/^HOOKS=/,/)/s/)$/ resume)/' /etc/mkinitcpio.conf
+ sudo mkinitcpio -P
+fi
 
 
+# nvidia
+sudo 
 
 
-==============
+# ============== POST INSTALLATION
 echo "set cookies from open.spotify.com in .config/sptlrx/config.yam"
 echo "\n"
 echp "autostart configuration for openvpn3 needed"
 
-swap 
-hibernate
 jupyter
