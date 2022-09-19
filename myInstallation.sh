5e85b2b6a60542878c62b24e241d5990565737ea
Saeed <39596095+smzm@users.noreply.github.com>
2022-09-19 09:06:27 +0000

    Update myInstallation.sh

--- myInstallation.sh
+++ myInstallation.sh
@@ -6,7 +6,7 @@ mkdir -p ~/tmpInstall
 
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed base-devel git wget yajl curl binutils
+sudo pacman -S base-devel git wget yajl curl binutils zsh nodejs npm yarn ruby python python-pip jupyter-notebook python-ipykernel pyenv neovim python-pynvim trash-cli lsd tmux aria2 broot ripgrep peco ack lostfiles github-cli tldr kitty zathura mpv xbindkeys xdotool thunar lxappearance xfce4-power-manager xfce4-settings nitrogen viewnior gtk3 ntfs-3g exfat-utils i3 python-i3ipc arandr rofi python-pywal calc jq bc wmctrl geoip dunst libnotify redshift imwheel numlockx unclutter unclutter fzy mediainfo xcolor conky --noconfirm
 
 
 # YAY
@@ -18,40 +18,35 @@ makepkg -si
 yay --save --nocleanmenu --nodiffmenu
 
 
-# ZSH
-sudo pacman -S zsh --noconfirm
+
+# yay packages
+yay -S xbindkeys_config-gtk2 fluent-reader udevil tailwind-css tailwindcss-language-server picom-ibhagwan-git polybar pacman-contrib cava ttf-indic-otf spotify sptlrx-bin scrot clipit nerd-fonts-inconsolata nerd-fonts-fira-code apple-fonts ttf-font-awesome vazirmatn-fonts noto-fonts-emoji
+
+
+# ZSH config
 sudo usermod -s /usr/bin/zsh $(whoami)
 sudo chsh -s $(which zsh)
-
 # OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 # zsh-auto-suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # starship
 curl -sS https://starship.rs/install.sh | sh
 cp $dotfiles/.config/starship.toml ~/.config/
-
 # zshrc 
 cp $dotfiles/.zshrc $dotfiles/.zshenv ~
 source ~/.zshrc
 
-# node
-sudo pacman -S nodejs npm yarn --noconfirm
+# node config
 mkdir ~/.npm
 npm config set prefix ~/.npm
 
-# ruby 
-sudo pacman -S ruby --noconfirm
+# ruby config
 echo "gem: --user-install" >> ~/.gemrc
 sudo gem update
 
-# Python
-sudo pacman -S python python-pip jupyter-notebook python-ipykernel --noconfirm
 
 # PIP, NPM, YARN
 pip install numpy pandas scipy sympy matplotlib plotly jedi python-language-server[all]
@@ -59,10 +54,8 @@ sudo npm i -g pyright vim-language-server vscode-langservers-extracted typescrip
 yarn global add yaml-language-server
 
 # NVIM
-yay -S tailwind-css tailwindcss-language-server
-sudo pacman -S pyenv neovim python-pynvim --noconfirm
 python3 -m pip install --user --upgrade pynvim
-sudo pip3 install pynvim --upgrade
+pip3 install pynvim --upgrade
 npm install -g neovim
 gem install neovim
 
@@ -71,22 +64,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 mkdir -p $HOME/.config/nvim
 yes | cp -rf $dotfiles/.config/nvim/* ~/.config/nvim/
 
-# Fonts
-yay -S nerd-fonts-inconsolata nerd-fonts-fira-code apple-fonts ttf-font-awesome vazirmatn-fonts noto-fonts-emoji
+# Fonts Config
 sudo cp $dotfiles/local.conf /etc/fonts/local.conf
-fc-cache -v
-
 # Install feather font
 mkdir -p $HOME/.fonts
 yes | cp -rf $dotfiles/.fonts/* $HOME/.fonts/
 fc-cache -fv
 
-# pacman packages
-sudo pacman -S trash-cli lsd tmux aria2 broot ripgrep peco ack lostfiles github-cli tldr kitty zathura mpv xbindkeys xdotool thunar lxappearance xfce4-power-manager xfce4-settings nitrogen viewnior gtk3 ntfs-3g exfat-utils --noconfirm
-
-
-# yay packages
-yay -S xbindkeys_config-gtk2 fluent-reader udevil
 
 # trash-cli
 sudo mkdir --parent /.Trash
@@ -125,14 +109,6 @@ cp $dotfiles/wall.jpg  $HOME/.config/
 # GTK-3.0
  yes | cp -rf $dotfiles/.config/gtk-3.0 $HOME/.config/
  
- 
- # packman packages
-sudo pacman -S i3 python-i3ipc arandr rofi python-pywal calc jq bc wmctrl geoip dunst libnotify redshift imwheel numlockx unclutter unclutter fzy mediainfo xcolor conky
-
-
-# yay packages
-yay -S picom-ibhagwan-git polybar pacman-contrib cava ttf-indic-otf spotify sptlrx-bin scrot clipit  
- 
 
 #i3
 yes | cp -rf $dotfiles/.config/i3 $HOME/.config/
@@ -163,5 +139,7 @@ cp $dotfiles/.imwheelrc  $HOME/
 
 # conky
 yes | cp -rf $dotfiles/.config/conky $HOME/.config/
+
+
 
 rm -rf ~/tmpInstall
