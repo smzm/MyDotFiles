fcc198509fe862e0c0ee9da6e614e6a14b46ce91
rodd <saeed.m.rodd@gmail.com>
2022-09-22 18:50:44 +0330

    update mydotfiles

--- .config/polybar/scripts/ipGeoLocation.sh
+++ .config/polybar/scripts/ipGeoLocation.sh
@@ -1,8 +1,10 @@
+#sleep 15
 #!/bin/bash
-sleep 15
 
 IP=`curl -w "\n" -s https://api.ipify.org`
-country=`geoiplookup "$IP" | awk 'BEGIN{FS=":"}; {print $2}' | awk 'BEGIN{FS=","}; {gsub(/[[:blank:]]/,""); print $1}'`
-#flag=`jq '.[] | select(.iso=='"$country"') .flag' ~/country.json 2> /dev/null` 
-#clean_flag=`echo "$flag" | awk '{gsub(/"/, "", $0) ; print $0}'`
-echo "$country" || echo "offline"
+if [[ ! $IP == "" ]] ;then
+    country=`geoiplookup "$IP" | awk 'BEGIN{FS=":"}; {print $2}' | awk 'BEGIN{FS=","}; {gsub(/[[:blank:]]/,""); print $1}'`
+    # flag=`jq '.[] | select(.iso=='"$country"') .flag' ~/country.json 2> /dev/null` 
+    # clean_flag=`echo "$flag" | awk '{gsub(/"/, "", $0) ; print $0}'`
+    echo "$country"
+fi
