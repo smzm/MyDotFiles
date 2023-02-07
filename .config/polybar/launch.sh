4a0ca84b0132f37452bff2e2e7b965d36e23f5ea
smzm <39596095+smzm@users.noreply.github.com>
2023-02-07 13:33:34 +0330

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Terminate already running bar instances
 killall -q polybar
-
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Wait some moment to i3 load completly
+sleep 5
 
 polybar main --config=~/.config/polybar/config.ini &
 polybar bottom  --config=~/.config/polybar/config.ini &
