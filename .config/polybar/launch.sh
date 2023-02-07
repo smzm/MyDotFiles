3c994ac483de15763b81dc41a3a3c0a3298c0106
smzm <39596095+smzm@users.noreply.github.com>
2023-02-07 13:34:19 +0330

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Wait some moment to i3 load completly
+# Wait multiple seconds to i3 load completly
 sleep 5
 
 polybar main --config=~/.config/polybar/config.ini &
