347f61447de607d9ce2df261573b629182fb0601
smzm <39596095+smzm@users.noreply.github.com>
2023-05-17 06:40:51 -0500

    Add files via upload

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -4,6 +4,13 @@ return {
 		require("auto-save").setup({
 			-- your config goes here
 			-- or just leave it empty :)
+			execution_message = {
+				message = function() -- message to print on save
+					return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+				end,
+				dim = 0.78, -- dim the color of `message`
+				cleaning_interval = 1000, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+			},
 		})
 	end,
 }
