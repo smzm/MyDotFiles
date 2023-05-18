4a280968587945a54ce4b6c23d34f36e22bde975
smzm <39596095+smzm@users.noreply.github.com>
2023-05-18 14:48:05 -0500

    Add files via upload

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,17 +1,23 @@
 return {
-    'ggandor/leap.nvim',
-    keys = {
-      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap forward to' },
-      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap backward to' },
-      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from windows' },
-    },
-    config = function(_, opts)
-      local leap = require('leap')
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ 'x', 'o' }, 'x')
-      vim.keymap.del({ 'x', 'o' }, 'X')
-    end,
+	"ggandor/leap.nvim",
+	keys = {
+		-- { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+		-- { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+	},
+	config = function(_, opts)
+		local leap = require("leap")
+		for k, v in pairs(opts) do
+			leap.opts[k] = v
+		end
+		leap.add_default_mappings(true)
+		vim.keymap.del({ "x", "o" }, "x")
+		vim.keymap.del({ "x", "o" }, "X")
+
+		-- Bidirectional Search
+		vim.keymap.set("n", "s", function()
+			local current_window = vim.fn.win_getid()
+			require("leap").leap({ target_windows = { current_window } })
+		end)
+	end,
 }
