37ecb884826f32667f8822ff2589e8c5a20d3b74
smzm <39596095+smzm@users.noreply.github.com>
2023-05-18 07:16:55 -0500

    Add files via upload

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,49 +1,39 @@
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
 
-
 -- use jk to exit insert mode
-map('i', 'jk', '<ESC>', opts)
-
+map("i", "jk", "<ESC>", opts)
 
 -- better up/down
-map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
-map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- disable hlsearch
-map('n', '<esc>', ":nohlsearch<CR>", {silent = true})
-
+map("n", "<esc>", ":nohlsearch<CR>", { silent = true })
 
 -- delete single character without copying into register
-map('n', 'x', '"_x', opts)
-map('v', 'x', '"_x', opts)
-
+map("n", "x", '"_x', opts)
+map("v", "x", '"_x', opts)
 
 -- (;) as (:)
 map("n", ";", ":", opts)
 
-
 -- increment/decrement numbers
-map('n', '<leader>+', '<C-a>', { desc = 'Increment number' })
-map('n', '<leader>-', '<C-x>', { desc = 'Decrement number' })
-
+map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- select all
-map('n', '<C-a>', 'gg<S-v>G', opts)
-
+map("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Set space as my leader key
 map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
 -- Add undo break-points
-map('i', ',', ',<c-g>u', opts)
-map('i', '.', '.<c-g>u', opts)
-map('i', ';', ';<c-g>u', opts)
-
+map("i", ",", ",<c-g>u", opts)
+map("i", ".", ".<c-g>u", opts)
+map("i", ";", ";<c-g>u", opts)
 
 -- Better split navigation
 map("n", "<C-h>", "<C-w>h", opts)
@@ -51,20 +41,19 @@ map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
-
 -- Resize splits with arrow keys
 map("n", "<M-l>", ":vertical resize +1<CR>", opts)
 map("n", "<M-h>", ":vertical resize -1<CR>", opts)
 map("n", "<M-k>", ":resize +1<CR>", opts)
 map("n", "<M-j>", ":resize -1<CR>", opts)
 
-
 -- Buffer navigation
 map("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
 map("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
-map("n", "[b", ":BufferLineCyclePrev<CR>", opts) map("n", "]b", ":BufferLineCycleNext<CR>", opts)
-map("n", "<Tab>", ":BufferLineCycleNext<CR>", opts)
-map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
+map("n", "[b", ":BufferLineCyclePrev<CR>", opts)
+map("n", "]b", ":BufferLineCycleNext<CR>", opts)
+-- map("n", "<Tab>", ":BufferLineCycleNext<CR>", opts)
+-- map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
 
 -- Move lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", opts)
@@ -74,7 +63,6 @@ map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", opts)
 map("v", "<A-j>", ":m '>+1<cr>gv=gv", opts)
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", opts)
 
-
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
 map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -83,21 +71,17 @@ map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 
-
-
-
 -- Visual
 -- better paste
-map('v', 'p', '"_dP', opts)
+map("v", "p", '"_dP', opts)
 
 -- move selected lines
-map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move down' })
-map('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move up' })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 
 -- stay in indent mode
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
-
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- Lazy keymap
 map("n", "<leader>Ll", ":Lazy<CR>", opts)
@@ -107,10 +91,8 @@ map("n", "<leader>LL", ":Lazy log<CR>", opts)
 map("n", "<leader>Lc", ":Lazy clean<CR>", opts)
 map("n", "<leader>Lp", ":Lazy profile<CR>", opts)
 
-
 -- Nvim tree
 map("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
-
 
 -- trouble.nvim keymaps
 map("n", "<leader>xx", ":TroubleToggle<CR>", opts)
@@ -120,11 +102,9 @@ map("n", "<leader>xq", ":TroubleToggle quickfix<CR>", opts)
 map("n", "<leader>xl", ":TroubleToggle loclist<CR>", opts)
 map("n", "<leader>xr", ":TroubleToggle lsp_references<CR>", opts)
 
-
 -- vim-illuminate
 map("n", "<leader>it", ":IlluminateToggle<CR>", opts)
 map("n", "<leader>ib", ":IlluminateToggleBuf<CR>", opts)
-
 
 -- Telescope keybindings
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
@@ -138,32 +118,27 @@ map("n", "<leader>fk", ":Telescope keymaps<CR>", opts)
 map("n", "<leader>fc", ":Telescope commands<CR>", opts)
 map("n", "<leader>fb", ":Telescope file_browser<CR>", opts)
 
-
 -- Telescope undo tree keymap
 map("n", "<leader>u", ":Telescope undo<CR>", opts)
-
 
 -- treesj keymaps
 map("n", "<leader>m", ":TSJToggle<CR>", opts)
 map("n", "<leader>s", ":TSJSplit<CR>", opts)
 map("n", "<leader>j", ":TSJJoin<CR>", opts)
 
-
 -- Todo comment
 vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
+	require("todo-comments").jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-  require("todo-comments").jump_prev()
+	require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
-
 
 -- Rust(cargo.toml) : Update Crates
 vim.keymap.set("n", "<leader>rcu", function()
-  require("crates").upgrade_all_crates()
-end, { desc = "Update All crates"})
-
+	require("crates").upgrade_all_crates()
+end, { desc = "Update All crates" })
 
 -- Rust Debugging
 -- Toggle Breakpoint
@@ -172,12 +147,10 @@ map("n", "<leader>dbs", "<cmd> RustDebuggables <CR>", { desc = "Open Panel" })
 map("n", "<leader>dbo", "<cmd> DapStepOver <CR>", { desc = "Dap Step Over" })
 -- Debugger Panel
 vim.keymap.set("n", "<leader>dbp", function()
-  local widgets = require('dap.ui.widgets')
-  local sidebar = widgets.sidebar(widgets.scopes)
-  sidebar.open()
-end, { desc = "Open Debugger Panel"})
-
-
+	local widgets = require("dap.ui.widgets")
+	local sidebar = widgets.sidebar(widgets.scopes)
+	sidebar.open()
+end, { desc = "Open Debugger Panel" })
 
 -- LSP Diagnostics Toggle
 map("n", "<leader>lt", ":ToggleDiag <CR>", { desc = "Toggle LSP Diagnostics" })
