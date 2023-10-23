bb63d1c5dfc132a69d8af0d1c7f11ded92c9d5c1
smzm <39596095+smzm@users.noreply.github.com>
2023-10-23 13:52:42 +0330

    Update indentline.lua

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,95 +1,54 @@
 return {
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {
-      char = '│',
-      filetype_exclude = {
-        'help',
-        'alpha',
-        'dashboard',
-        'NvimTree',
-        'Trouble',
-        'lazy',
-        'TelescopePrompt',
-        'text',
-        'toggleterm',
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-      context_highlight_list = { 'SpecialKey', 'Operator', 'Statement' },
-    },
-  },
-  {
-    'echasnovski/mini.indentscope',
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      symbol = '│',
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'help', 'alpha', 'dashboard', 'NvimTree', 'Trouble', 'lazy', 'mason' },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-    config = function(_, opts)
-      require('mini.indentscope').setup(opts)
-    end,
-  }
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+
+			local hooks = require("ibl.hooks")
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+			require("ibl").setup({
+				indent = { highlight = highlight, tab_char = "" },
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+		config = function(_, opts)
+			require("mini.indentscope").setup(opts)
+		end,
+	},
 }
-
-
-
--- Indent Line with another configuration
--- return {
---     'lukas-reineke/indent-blankline.nvim',
---     event = {
---         'BufReadPost',
---         'BufNewFile',
---     },
---     opts = {
---         char = '▏',
---         context_char = '▏',
---         use_treesitter = true,
---         show_first_indent_level = true,
---         show_trailing_blankline_indent = false,
---         show_current_context = true,
---         show_current_context_start = true,
---         filetype_exclude = {
---             'help',
---             'NvimTree',
---             'dashboard',
---             'Trouble',
---             'neogitstatus',
---         },
---         context_patterns = {
---             'class',
---             'return',
---             'function',
---             'method',
---             '^if',
---             '^while',
---             'jsx_element',
---             '^for',
---             '^object',
---             '^table',
---             'block',
---             'arguments',
---             'if_statement',
---             'else_clause',
---             'jsx_element',
---             'jsx_self_closing_element',
---             'try_statement',
---             'catch_clause',
---             'import_statement',
---             'operation_type',
---         },
---     },
--- }
-
-
-
